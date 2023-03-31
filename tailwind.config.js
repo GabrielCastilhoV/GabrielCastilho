c52211ca65f5a7195619368abe8c1ecfb2a24910
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2023-03-30 20:42:51 -0400

    style: configure tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,19 @@
+const { fontFamily } = require("tailwindcss/defaultTheme")
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
   content: ["src/app/*.tsx", "src/components/**/*.tsx"],
-};
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...fontFamily.sans],
+      },
+      colors: {
+        dark: "#111010",
+        green: "#24b36b",
+        white: "#FEFFEA",
+      },
+    },
+  },
+}
