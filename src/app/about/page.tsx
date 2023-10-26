a83c4507a65081e22d14e097e9ab62ea8ee95283
GabrielCastilhoV <gabrielcastilho70@gmail.com>
2023-10-26 16:05:22 -0400

    feat(about-page): add content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,28 @@
 export default function About() {
-  return <p className="text-slate-50">Coming soon</p>
+  return (
+    <div>
+      <p className="mt-5 text-grey">
+        Founder of{" "}
+        <a
+          href="https://juridiq.com.br"
+          className="text-green"
+          style={{ textDecoration: "underline" }}
+        >
+          Juridiq
+        </a>{" "}
+        and Developer with over 4 years of experience.
+      </p>
+
+      <p className="mt-5 text-grey">
+        During these years, I have worked on several projects, such as:
+        Blockchain, E-commerce, Mobile Applications, SAAS and others.
+      </p>
+
+      <p className="mt-5 text-grey">
+        I have experience with team leadership and project management, and
+        during these years, the main technologies I used were Next.JS and
+        Node.JS
+      </p>
+    </div>
+  )
 }
